bb50abb0eb8d2061b1ddc7460185a5b945cc8a3a
Carolina Nonato <csnonato@gmail.com>
2022-02-13 19:44:54 -0500

    card component + replication

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './style.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import katie from "./images/katie.png"
+import wedding from "./images/wedding.png"
+import bike from "./images/bike.png"
+
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      {/* <Hero /> */}
-      <Card />
-    </div>
+      <Hero />
+      <div className="test">
+        <Card src={katie} rating={6} text={"Life lessons with Katie Zaferes"} price={136} />
+        <Card src={wedding} rating={6} text={"Learn wedding photography"} price={136} />
+        <Card src={bike} rating={6} text={"Group Mountain Biking"} price={136} />
+      </div>
+    </div >
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,19 @@
 import React from "react";
 import star from "../images/star.png"
-import katie from "../images/katie.png"
 
-export default function Card() {
+
+export default function Card({ src, rating, text, price }) {
     return (
         <div className="card">
-            <img className="card--image" src={katie} alt="" />
+            <img className="card--image" src={src} alt="" />
             <div className="card--stats">
                 <img className="card--star" src={star} alt="" />
                 <span>5.0</span>
-                <span className="gray">(6) · </span>
-                <span className="gray">USA</span>
+                <span className="gray">({rating}) · </span>
+                <span className="gray"> USA</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold" >From $136 </span>/ person</p>
+            <p>{text}</p>
+            <p><span className="bold" >From ${price} </span>/ person</p>
         </div>
     )
 }
